95a743af00cb9d103edf23600329d279109c3770
Kirill Voronkov <vorronkovkirill@gmail.com>
2023-11-28 22:31:37 +0200

    new tricks

---
+++ src/test/java/BrowsingTesting.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class BrowsingTesting {
+
+    WebDriver wd;
+
+
+    @Test
+    public void test(){
+        wd = new ChromeDriver();
+
+        wd.navigate().to("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+    }
+}
